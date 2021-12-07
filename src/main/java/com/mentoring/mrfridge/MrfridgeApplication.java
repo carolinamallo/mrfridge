86be171b8fd225ea8953871abae669704e74752c
Nelson Jumbo <njumbo@thoughtworks.com>
2021-12-07 09:56:15 +0100

    First commit

---
+++ src/main/java/com/mentoring/mrfridge/MrfridgeApplication.java
@@ -0,0 +1,13 @@
+package com.mentoring.mrfridge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MrfridgeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MrfridgeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mentoring/mrfridge/MrfridgeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mentoring.mrfridge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MrfridgeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
